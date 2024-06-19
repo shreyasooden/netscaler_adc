8385460e6bd78fa0b01745ce023ce44a57ba96ed
shreyasooden <38000303+shreyasooden@users.noreply.github.com>
2024-06-19 11:28:10 +0530

    Update datetime_filters.py

--- filter_plugins/datetime_filters.py
+++ filter_plugins/datetime_filters.py
@@ -3,8 +3,7 @@ from datetime import datetime
 def compare_datetimes(datetime1, datetime2, datetime3, format="%Y-%m-%d %H:%M:%S"):
     dt1 = datetime.strptime(datetime1, format)
     dt2 = datetime.strptime(datetime2, format)
-    dt3 = datetime.strptime(datetime3, format)
-    return dt1 < dt3 < dt2
+    return dt1 < dt2
 
 class FilterModule(object):
     def filters(self):
