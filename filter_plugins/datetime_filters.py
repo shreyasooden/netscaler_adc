6be7a0f22e5b435d520618a50dbec723945d1382
shreyasooden <38000303+shreyasooden@users.noreply.github.com>
2024-06-19 11:42:04 +0530

    Update datetime_filters.py

--- filter_plugins/datetime_filters.py
+++ filter_plugins/datetime_filters.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def compare_datetimes(datetime1, datetime2, datetime3, format="%Y-%m-%d %H:%M:%S"):
+def compare_datetimes(datetime1, datetime2, format="%Y-%m-%d %H:%M:%S"):
     dt1 = datetime.strptime(datetime1, format)
     dt2 = datetime.strptime(datetime2, format)
     return dt1 < dt2
